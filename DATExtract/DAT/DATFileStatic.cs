4dc517ebee9ed139b85e56aed645f553e86f0b72
Connor Harrison <connorh315@gmail.com>
2022-05-30 09:12:43 +0100

    Add project files.

---
+++ DATExtract/DAT/DATFileStatic.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModLib;
+using System.Diagnostics;
+
+namespace DATExtract
+{
+    public partial class DATFile
+    {
+        private static long CRC_FNV_OFFSET_32 = 0x811c9dc5;
+
+        private static long CRC_FNV_PRIME_32 = 0x199933;
+
+        private static long CRC_FNV_OFFSET_64 = -3750763034362895579;
+
+        private static long CRC_FNV_PRIME_64 = 1099511628211;
+
+        public static DATFile Open(string location)
+        {
+            DATFile dat = new DATFile();
+            dat.fileLocation = location;
+
+            using (ModFile file = ModFile.Open(location))
+            {
+                uint hdrOffset = file.ReadUint();
+                if ((hdrOffset & 0x80000000) != 0)
+                {
+                    hdrOffset ^= 0xffffffff;
+                    hdrOffset <<= 8;
+                    hdrOffset += 0x100;
+                }
+                dat.hdrOffset = hdrOffset;
+
+                uint hdrSize = file.ReadUint();
+                dat.hdrSize = hdrSize;
+
+                using (ModFile hdrBlock = file.LoadSegment((long)hdrOffset, (int)hdrSize))
+                {
+                    dat.hdrBlock = hdrBlock;
+
+                    int firstValue = hdrBlock.ReadInt();
+                    uint secondValue = hdrBlock.ReadUint();
+                    
+                    if (secondValue != 0x3443432e && secondValue != 0x2e434334 && firstValue != 0x3443432e && firstValue != 0x2e434334)
+                    {
+                        dat.archiveId = firstValue;
+                        dat.fileCount = secondValue;
+
+                        dat.LoadAsOld();
+                    }
+                    else
+                    {
+                        dat.LoadAsNew();
+                    }
+                }
+
+            }
+
+            return dat;
+        }
+    }
+}
---
+++ DATExtract/DAT/Types/Shared.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DATExtract
+{
+    public partial class DATFile
+    {
+        // GetFileID took ridiculously long if I didn't do a lookup
+        private Dictionary<long, int> crcTranslation = new();
+
+        private int GetFileID(int thisId, string fullname)
+        {
+            if (archiveId == -1)
+            {
+                return (int)fileCount - 1 - thisId;
+            }
+
+            long crc = is64 ? CRC_FNV_OFFSET_64 : CRC_FNV_OFFSET_32;
+            fullname = fullname.Substring(1);
+            foreach (char character in fullname.ToUpper())
+            {
+                crc ^= character;
+                crc *= is64 ? CRC_FNV_PRIME_64 : CRC_FNV_PRIME_32;
+            }
+
+            if (!is64)
+            {
+                crc &= 0xffffffff;
+            }
+
+            if (crcTranslation.ContainsKey(crc))
+            {
+                return crcTranslation[crc];
+            }
+
+            for (int i = 0; i < files.Length; i++) // I think that despite having to reloop, this will still be quicker in the long run
+            {
+                if (files[i].path == fullname)
+                {
+                    return i;
+                }
+            }
+
+            Console.WriteLine("Could not find CRC of file: {0}", fullname);
+
+            return 0;
+        }
+
+        private void GetCRCs()
+        {
+            long currentOffset = hdrBlock.Position;
+            hdrBlock.Seek(nameSignOffset + (fileCount * 4), SeekOrigin.Begin);
+            uint test = hdrBlock.ReadUint();
+            if (test != 0 && archiveId <= -8)
+            {
+                is64 = true;
+            }
+
+            hdrBlock.Seek(currentOffset, SeekOrigin.Begin);
+
+            Console.WriteLine("64-bit archive: " + is64);
+
+            bool checkForCollisions = false;
+
+            for (int i = 0; i < fileCount; i++)
+            {
+                if (is64)
+                {
+                    files[i].crc = hdrBlock.ReadLong(true);
+                }
+                else
+                {
+                    files[i].crc = hdrBlock.ReadUint();
+                }
+
+                //if (crcTranslation.ContainsKey(files[i].crc))
+                //{
+                //    Console.WriteLine(files[i].crc);
+                //}
+
+                crcTranslation[files[i].crc] = i;
+
+                if (files[i].crc == 0)
+                {
+                    checkForCollisions = true;
+                }
+            }
+
+            if (checkForCollisions == false) return;
+
+            uint collisionFiles = hdrBlock.ReadUint();
+            uint collisionNamesSize = hdrBlock.ReadUint();
+            if (collisionFiles > 0)
+            {
+                Console.WriteLine("CRC collisions detected for {0} files, fixing...", collisionFiles);
+                for (int i = 0; i < collisionFiles; i++)
+                {
+                    string path = hdrBlock.ReadNullString();
+                    hdrBlock.Seek(3, SeekOrigin.Current);
+                    files[i].path = path;
+                }
+            }
+        }
+    }
+}
